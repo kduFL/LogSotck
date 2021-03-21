73926c2e69917839f1138ab3007de17870b41776
kdu <kdu.eduardo97@gmail.com>
2021-03-21 15:06:28 -0300

    Fix: Passa stock para json

--- src/app/App.java
+++ src/app/App.java
@@ -19,209 +19,205 @@ import java.util.Scanner;
 
 public class App {
 
-		public static void start(Scanner input){
-			try {
-				System.out.println("\n1. Entrar\n2. Criar nova conta\n0. Sair");
-				System.out.print("> ");
-				int number = input.nextInt();
-	
-				if(number == 1) {
-					login(input);
-	
-					//Stock stock = new Stock();
-					
-					//stock.addItem(input);
-					//stock.getItems();
-	
-					//stock.removeItem(input);
-					//stock.getItems();
-				} else if( number == 2) {
-					createAccount(input);
-					start(input);
-				} else if( number == 0) {
-					System.out.println("\nSaindo...");
-					System.exit(0);
-				} else {
-					System.out.println("Voê digitou um número inválido. Tente novamente.\n");
-					start(input);
-				}
-			} catch (InputMismatchException err) {
-				System.out.println("Opss... Você digitou caractere.\n");
-				input.nextLine(); //Limpa cache
+	public static void start(Scanner input) {
+		try {
+			System.out.println("\n1. Entrar\n2. Criar nova conta\n0. Sair");
+			System.out.print("> ");
+			int number = input.nextInt();
+
+			if (number == 1) {
+				login(input);
+
+				// Stock stock = new Stock();
+
+				// stock.addItem(input);
+				// stock.getItems();
+
+				// stock.removeItem(input);
+				// stock.getItems();
+			} else if (number == 2) {
+				createAccount(input);
+				start(input);
+			} else if (number == 0) {
+				System.out.println("\nSaindo...");
+				System.exit(0);
+			} else {
+				System.out.println("Voê digitou um número inválido. Tente novamente.\n");
 				start(input);
 			}
+		} catch (InputMismatchException err) {
+			System.out.println("Opss... Você digitou caractere.\n");
+			input.nextLine(); // Limpa cache
+			start(input);
 		}
+	}
 
-		public static void login(Scanner input) {
-			System.out.print("\nID: ");
-			int id = input.nextInt();
+	public static void login(Scanner input) {
+		System.out.print("\nID: ");
+		int id = input.nextInt();
 
-			checkUserDataExists(input, id);
-		}
+		checkUserDataExists(input, id);
+	}
 
-		public static void menu(Scanner input){
-			Stock stock = new Stock();
+	public static void menu(Scanner input) {
+		Stock teste = new Stock();
+		teste.verificar();
 
-			while (true) {
-				stock.getItems();
-								
-				System.out.println("\n1. Adiconar");
-				System.out.println("2. Remover");
+		while (true) {
+			teste.getItens();
 
-				System.out.print("> ");
-				int option = input.nextInt();
-
-				if (option == 1) {
-					stock.addItem(input);
-				} else if (option == 2){
-					stock.removeItem(input);
-				} else {
-					System.out.println("Opção invalida!");
-				}
+			System.out.print("\n1. Adicionar: ");
+			int i = input.nextInt();
+			if (i == 1) {
+				teste.addItem(input);
 			}
 		}
+	}
 
-		public static void createAccount(Scanner input) {
-			System.out.println("\n\nCriar sua conta\n");
-			System.out.println("Você é: \n  1. Gerente de Estoque\n  2. Auxiliar de Estoque");
-			
-			try {	
-				int job = input.nextInt();
-				
-				if(job == 1) {
-					Manager manager = new Manager();
-					
-					registerEmployee(input, manager, job);
-				} else if( job == 2) {
-					System.out.println("Assistente");
-				} else {
-					System.out.println("\nOps... Você digitu o número errado. Digite apenas 1. Gerente ou 2. Assistente.\n");
-				}
-			} catch (InputMismatchException err) {
-				System.out.println("\nOps... Você digitou um caractere. Digite apenas 1. Gerente ou 2. Assistente.");
-				input.nextLine(); //Esvazia buffer do input
-			}
-		}
+	public static void createAccount(Scanner input) {
+		System.out.println("\n\nCriar sua conta\n");
+		System.out.println("Você é: \n  1. Gerente de Estoque\n  2. Auxiliar de Estoque");
 
-		public static void registerEmployee(Scanner input, Employee employee, int job) {
-				input.nextLine(); //Esvazia buffer do teclado para usar o nextLine nas próximos inputs
+		try {
+			int job = input.nextInt();
 
-				System.out.println("\n\n===== Cadastro =====");
-				
-				System.out.println("\nDigite seu nome: ");
-				employee.setName(input.nextLine());
-				
-				employee.setPosition(job);
-				
-				int id = createID();
+			if (job == 1) {
+				Manager manager = new Manager();
 
-				employee.setId(id);
-
-				saveDate(employee);
-
-				System.out.println("\n\nSeu ID para entrar na plataforma: " + employee.getId());
-
-				System.out.println("\nCadastro realizado com sucesso!\n\n");
-
-				System.out.println("\nVocê será redirecionado para tela inicial.\n");
-		}
-
-		public static int createID() {
-			Random rand = new Random();
-
-			return rand.nextInt(10000);
-		}
-
-		public static void saveDate(Employee employee) {
-			File dataFile = new File("userData.json"); 
-
-			if(dataFile.exists()) {
-				try {
-					String jsonFile = new String(Files.readAllBytes(Paths.get("userData.json")));
-
-					JSONObject userData = new JSONObject(jsonFile);
-
-					addDataToFile(userData, employee);
-
-				} catch(IOException e) {
-					e.printStackTrace();
-				}
+				registerEmployee(input, manager, job);
+			} else if (job == 2) {
+				System.out.println("Assistente");
 			} else {
-				JSONObject userData = new JSONObject();
+				System.out.println("\nOps... Você digitu o número errado. Digite apenas 1. Gerente ou 2. Assistente.\n");
+			}
+		} catch (InputMismatchException err) {
+			System.out.println("\nOps... Você digitou um caractere. Digite apenas 1. Gerente ou 2. Assistente.");
+			input.nextLine(); // Esvazia buffer do input
+		}
+	}
+
+	public static void registerEmployee(Scanner input, Employee employee, int job) {
+		input.nextLine(); // Esvazia buffer do teclado para usar o nextLine nas próximos inputs
+
+		System.out.println("\n\n===== Cadastro =====");
+
+		System.out.println("\nDigite seu nome: ");
+		employee.setName(input.nextLine());
+
+		employee.setPosition(job);
+
+		int id = createID();
+
+		employee.setId(id);
+
+		saveDate(employee);
+
+		System.out.println("\n\nSeu ID para entrar na plataforma: " + employee.getId());
+
+		System.out.println("\nCadastro realizado com sucesso!\n\n");
+
+		System.out.println("\nVocê será redirecionado para tela inicial.\n");
+	}
+
+	public static int createID() {
+		Random rand = new Random();
+
+		return rand.nextInt(10000);
+	}
+
+	public static void saveDate(Employee employee) {
+		File dataFile = new File("userData.json");
+
+		if (dataFile.exists()) {
+			try {
+				String jsonFile = new String(Files.readAllBytes(Paths.get("userData.json")));
+
+				JSONObject userData = new JSONObject(jsonFile);
 
 				addDataToFile(userData, employee);
-			}
-		}
 
-		public static void addDataToFile(JSONObject userData, Employee employee) {
-		
-			try {
-				Map data = new LinkedHashMap<>(2);
-				
-				data.put("nome", employee.getName());
-				data.put("cargo", employee.getPosition());
-
-				userData.put(Integer.toString(employee.getId()), data);
-				
-				FileWriter file = new FileWriter("userData.json");
-
-				file.write(userData.toString(2));
-				file.close();
-
-			} catch(IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		} else {
+			JSONObject userData = new JSONObject();
+
+			addDataToFile(userData, employee);
 		}
+	}
 
-		public static void checkUserDataExists(Scanner input, int id) {
-			File file = new File("userData.json"); 
-			
-			if(file.exists()) {
-				getUserData(input, id);
-			} else {
-				System.out.println("\nOpss... Não temos nenhum usuário cadastrado no LogStock. \nVocê será redirecionado para tela inicial para realizar o cadastro.\n");
-				start(input);
-			}
+	public static void addDataToFile(JSONObject userData, Employee employee) {
+
+		try {
+			Map data = new LinkedHashMap<>(2);
+
+			data.put("nome", employee.getName());
+			data.put("cargo", employee.getPosition());
+
+			userData.put(Integer.toString(employee.getId()), data);
+
+			FileWriter file = new FileWriter("userData.json");
+
+			file.write(userData.toString(2));
+			file.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
 
-		public static void getUserData(Scanner input, int id) {
-			try {
-				String data = new String(Files.readAllBytes(Paths.get("userData.json")));
+	public static void checkUserDataExists(Scanner input, int id) {
+		File file = new File("userData.json");
 
-				JSONObject userData = new JSONObject(data);
-
-				String idString = Integer.toString(id);
-
-				JSONObject idData = userData.getJSONObject(idString);
-				
-				showUser(idData);
-
-				menu(input);
-
-			} catch (IOException err) {
-				err.printStackTrace();
-			} catch (JSONException err) {
-				System.out.println("\nID não encontrado. \nVocê será redirecionado para tela inicial para realizar o cadastro.\n");
-				start(input);
-			}
-		}
-
-		public static void showUser(JSONObject idData) {
-			System.out.print("\n\nUsuário: " + idData.getString("nome") + "\n");
-			int position = idData.getInt("cargo");
-
-			if(position == 1) {
-				System.out.println("Cargo: Gerente de Estoque");
-			} else {
-				System.out.println("Cargo: Auxiliar de Estoque");
-			}
-
-		}
-    public static void main(String[] args) throws Exception {
-			Scanner input = new Scanner(System.in);
-			
-			System.out.println("\n========== LogStock ==========");
-
+		if (file.exists()) {
+			getUserData(input, id);
+		} else {
+			System.out.println(
+					"\nOpss... Não temos nenhum usuário cadastrado no LogStock. \nVocê será redirecionado para tela inicial para realizar o cadastro.\n");
 			start(input);
-    }
+		}
+	}
+
+	public static void getUserData(Scanner input, int id) {
+		try {
+			String data = new String(Files.readAllBytes(Paths.get("userData.json")));
+
+			JSONObject userData = new JSONObject(data);
+
+			String idString = Integer.toString(id);
+
+			JSONObject idData = userData.getJSONObject(idString);
+
+			showUser(idData);
+
+			menu(input);
+
+		} catch (IOException err) {
+			err.printStackTrace();
+		} catch (JSONException err) {
+			System.out
+					.println("\nID não encontrado. \nVocê será redirecionado para tela inicial para realizar o cadastro.\n");
+			start(input);
+		}
+	}
+
+	public static void showUser(JSONObject idData) {
+		System.out.print("\n\nUsuário: " + idData.getString("nome") + "\n");
+		int position = idData.getInt("cargo");
+
+		if (position == 1) {
+			System.out.println("Cargo: Gerente de Estoque");
+		} else {
+			System.out.println("Cargo: Auxiliar de Estoque");
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("\n========== LogStock ==========");
+
+		start(input);
+	}
 }
--- src/app/Stock.java
+++ src/app/Stock.java
@@ -1,22 +1,49 @@
-import java.util.ArrayList;
+import org.json.*;
+import java.io.File;
 import java.util.Scanner;
+import java.util.Random;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Stock {
+    JSONArray jsonList = new JSONArray();
+    FileWriter writeFile = null;
 
-    ArrayList<Item> stock = new ArrayList<Item>();
+    public void verificar() {
+        File dataFile = new File("data.json");
+        if (dataFile.exists()) {
+            try {
+                String testeList = new String(Files.readAllBytes(Paths.get("data.json")));
+                JSONArray teste = new JSONArray(testeList);
 
-    public void stock() {
-        
-        getItems();
+                int size = teste.length();
 
+                for (int i = 0; i < size; i++) {
+                    jsonList.put(teste.getJSONObject(i));
+                }
+
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public void getItens() {
+        int size = jsonList.length();
+        for (int i = 0; i < size; i++) {
+            System.out.println(jsonList.getJSONObject(i));
+        }
     }
 
     public void addItem(Scanner input) {
-        Item item = new Item();
-        
-        System.out.print("ID: ");
-        int id = input.nextInt();
+        JSONObject item = new JSONObject();
 
+        Random rand = new Random();
+        int id = rand.nextInt(10000);
+
+        System.out.println("\nDADOS DO ITEM\n");
         System.out.print("Marca: ");
         String brand = input.next();
 
@@ -32,34 +59,23 @@ public class Stock {
         System.out.print("Estado: ");
         String estado = input.next();
 
-        item.createItem(id, brand, quantity, validity, description, estado);
-        stock.add(item);
-    }
+        item.put("id", id);
+        item.put("marca", brand);
+        item.put("quantidade", quantity);
+        item.put("validade", validity);
+        item.put("descricao", description);
+        item.put("estado", estado);
 
-    public void getItems() {
-        System.out.println("");
-        for (int i = 0; i < stock.size(); i++) {
-            int id = stock.get(i).getId();
-            String marca = stock.get(i).getBrand();
-            int quantity = stock.get(i).getQuantity();
-            String validity = stock.get(i).getValidity();
-            String description = stock.get(i).getDescription();
-            String estado = stock.get(i).getEstado();
+        jsonList.put(item);
 
-            System.out.println("ID: " +id + " | Marca: " + marca + " | Quantidade: " + quantity + "x | Validade: " + validity + " | Descrição: " + description + " | Estado: " + estado + "\n");
+        try {
+            writeFile = new FileWriter("data.json");
+            writeFile.write(jsonList.toString(2));
+            writeFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 
-    public void removeItem(Scanner input){
-        System.out.print("\nID: ");
-        int index = input.nextInt();
-
-        for (int i = 0; i < stock.size(); i++) {
-            int id = stock.get(i).getId();
-
-            if (id == index) {
-                stock.remove(i);
-            }
-        }
-    }
 }
